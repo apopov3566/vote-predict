5ca5eb5315a3378c56b98e8b22de6b8efe5a0610
Andrew Hess <ahess3815@gmail.com>
2019-02-11 23:11:14 -0800

    Altered network design to give better accuracy

--- nnetwork.py
+++ nnetwork.py
@@ -11,42 +11,85 @@ import datahandler
 def main():
     ''' Train and evaluate a neural network model. '''
 
-    # Get the training and test data.
-    x_train, y_train, x_test, y_test = datahandler.load_train("data/train_2008.csv", 1000, 4000)
+    # Get the training and validation data.
+    x_train, y_train, x_val, y_val, test1, test2 = datahandler.load_all(10000, True)
     print('x train shape:', x_train.shape)
-    print('x test shape:', x_test.shape)
+    print('x test shape:', x_val.shape)
 
-    # Train and evaluate the model.
-    model = train_network(x_train, y_train, x_test, y_test)
+    # best_test_acc = 0
+    # best_dropouts = []
+    # best_reg_pow = None
+    #
+    # for drop1 in np.linspace(0, 0.9, 10):
+    #     for drop2 in np.linspace(0, 0.9, 10):
+    #         for reg_pow in range(-1, -6, -1):
+    #             # Train and evaluate the model.
+    #             model = setup_network(x_train.shape[1:], [drop1, drop2], reg_pow)
+    #             model = train_network(x_train, y_train, x_val, y_val, model, plot=False)
+    #             print(f'dropouts: {[drop1, drop2]} \treg_power: {reg_pow}')
+    #             print('train accuracy:', evaluate_network(model, x_train, y_train))
+    #
+    #             test_acc = evaluate_network(model, x_val, y_val)
+    #             print('test accuracy:', test_acc)
+    #
+    #             # Update the best parameters so far.
+    #             if test_acc > best_test_acc:
+    #                 best_test_acc = test_acc
+    #                 best_dropouts = [drop1, drop2]
+    #                 best_reg_pow = reg_pow
+    #
+    #             # Show the best hyperpameters so far.
+    #             print('best hyperpameters so far:')
+    #             print('-------------------------')
+    #             print('best test acc:', best_test_acc)
+    #             print('best dropouts:', best_dropouts)
+    #             print('best reg pow:', best_reg_pow)
+
+    drop1 = 0.2
+    drop2 = 0.1
+    reg_pow = -6
+    model = setup_network(x_train.shape[1:], [drop1, drop2], reg_pow)
+    model = train_network(x_train, y_train, x_val, y_val, model, epochs=20)
+    print(f'dropouts: {[drop1, drop2]} \treg_power: {reg_pow}')
     print('train accuracy:', evaluate_network(model, x_train, y_train))
-    print('test accuracy:', evaluate_network(model, x_test, y_test))
 
+    test_acc = evaluate_network(model, x_val, y_val)
+    print('test accuracy:', test_acc)
+
+    datahandler.save_model(model, "nnetwork4.model")
+    datahandler.save_prediction(np.ndarray.flatten(model.predict(test1)),"data/nnetwork2008_v4")
+    datahandler.save_prediction(np.ndarray.flatten(model.predict(test2)),"data/nnetwork2012_v4")
     return
 
 
-def setup_network(input_shape):
+def setup_network(input_shape, dropouts, reg_pow):
     ''' Create a neural network to predict probability of voting. '''
 
-    L2 = regularizers.l2(0.01)
+    L2 = regularizers.l2(10 ** reg_pow)
 
     # Setup the model.
     model = Sequential()
-    model.add(Dense(100, input_shape=input_shape, activation='relu', kernel_regularizer=L2))
-    model.add(Dropout(0.7))
-    model.add(Dense(75, activation='relu', kernel_regularizer=L2))
-    model.add(Dropout(0.2))
-    model.add(Dense(10, activation='relu'))
+    model.add(Dense(400, input_shape=input_shape, activation='relu', kernel_regularizer=L2))
+    model.add(Dropout(dropouts[0]))
+    model.add(Dense(50, activation='relu', kernel_regularizer=L2))
+    model.add(Dropout(dropouts[1]))
+    model.add(Dense(50, activation='relu', kernel_regularizer=L2))
+    model.add(Dropout(dropouts[1]))
+    model.add(Dense(20, activation='relu'))
     model.add(Dense(1, activation='sigmoid'))
+
+    model.summary()
 
     return model
 
 
-def train_network(x, y, val_x, val_y, epochs=50, plot=True):
+def train_network(x, y, val_x, val_y, model=None, epochs=5, plot=True):
     ''' Setup and train a neural network. '''
 
-    # Creat the network.
-    model = setup_network(x.shape[1:])
-    model.summary()
+    if model is None:
+        # Creat the network.
+        print('creating a new network ...')
+        model = setup_network(x.shape[1:])
 
     # Train the network.
     model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
@@ -62,7 +105,7 @@ def train_network(x, y, val_x, val_y, epochs=50, plot=True):
 
 
 def evaluate_network(model, x, y):
-    ''' Evaluate the model on the given data using 0/1 loss. '''
+    ''' Evaluate the model on the given data using AUC loss. '''
 
     # Get the model predictions.
     y_pred = np.ndarray.flatten(model.predict(x))
